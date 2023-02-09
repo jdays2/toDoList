8cc664aa85e82c8c94cbf4f8d6d11b735be898e9
MarkHW <lolka-pop@rambler.ru>
2023-02-09 23:31:39 +0300

    draggable :3

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -3,6 +3,9 @@ import Task from "./Task/Task";
 import addNewElement from "../../assets/img/createNewElement.svg";
 import { AiOutlineCloseSquare } from "react-icons/ai";
 import { useDispatch } from "react-redux";
+import Draggable from "react-draggable";
+
+import React from "react";
 
 import { deleteSomeTasks, getItemForm } from "./../../redux/slices/formSlice";
 import ItemForm from "../Form/ItemForm/ItemForm";
@@ -10,30 +13,56 @@ import ItemForm from "../Form/ItemForm/ItemForm";
 function TaskList({ items, title, id, created }) {
   const dispatch = useDispatch();
 
+  const dragStartHandler = (event, e) => {
+    event.preventDefault();
+    console.log("drag", e);
+  };
+  const dragOverHandler = (e) => {};
+  const dragEndHandler = (e) => {
+    e.preventDefault();
+  };
+  const dropHandler = (event, e) => {
+    event.preventDefault();
+    console.log("drag", e);
+  };
   return (
-    <div className={styles.list__wrapper}>
-      <div className={styles.item}>
-        <img
-          className={styles.addNew}
-          src={addNewElement}
-          onClick={() => {
-            dispatch(getItemForm({ id, value: !created }));
-          }}
-        />
-        <AiOutlineCloseSquare
-          className={styles.deleteElement}
-          onClick={() => dispatch(deleteSomeTasks(id))}
-        />
-        <p className={styles.title}>{title}</p>
-        {items &&
-          items.map((e, i) => {
-            return (
-              <Task value={e.value} key={i} id={i} idList={id} done={e.done} />
-            );
-          })}
-        {created ? <ItemForm idList={id} created={created} /> : null}
+    <Draggable>
+      <div className={styles.list__wrapper}>
+        <div className={styles.item}>
+          <img
+            className={styles.addNew}
+            src={addNewElement}
+            onClick={() => {
+              dispatch(getItemForm({ id, value: !created }));
+            }}
+          />
+          <AiOutlineCloseSquare
+            className={styles.deleteElement}
+            onClick={() => dispatch(deleteSomeTasks(id))}
+          />
+          <p className={styles.title}>{title}</p>
+
+          {items &&
+            items.map((e, i) => {
+              return (
+                <Task
+                  draggable={true}
+                  onDragEnd={(event) => dragEndHandler(event)}
+                  onDragOver={(event) => dragOverHandler(event)}
+                  onDrop={(event) => dropHandler(event, e)}
+                  value={e.value}
+                  key={i}
+                  id={i}
+                  idList={id}
+                  done={e.done}
+                />
+              );
+            })}
+
+          {created ? <ItemForm idList={id} created={created} /> : null}
+        </div>
       </div>
-    </div>
+    </Draggable>
   );
 }
 
