10749e3395a1d9843987535b62f706731c78667c
MarkHW <lolka-pop@rambler.ru>
2023-02-10 14:14:54 +0300

    !

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import "./App.css";
 import TaskList from "./components/TaskList/TaskList";
 import Header from "./components/Header/Header";
+import videoBg from "./../src/assets/vid/video.mp4";
 
 import Form from "./components/Form/TaskForm";
 import { useSelector } from "react-redux";
+import { BsPlusSquareFill } from "react-icons/bs";
 
 function App() {
   const tasks = useSelector((state) => state.form.tasks);
   return (
     <div className="App">
-      <Header />
-      <Form />
-      <div className="content__wrapper">
-        {tasks.map((e, i) => {
-          return (
-            <TaskList
-              title={e.title}
-              items={e.items}
-              key={i}
-              id={i}
-              created={e.isCreated}
-            />
-          );
-        })}
+      <video src={videoBg} autoPlay muted loop />
+      <div class="wrapper">
+        <Header />
+        <Form />
+        <button className="tryMeButton">
+          <span>THE NEW LIFE ORDER.</span>
+          <BsPlusSquareFill className="addIcon" />
+        </button>
+        <p className="discription">
+          Try to organize your day and make a clear plan of action. You will be
+          able to organize and consciously approach your life, making it more
+          orderly and under your control. With this preparation, you can do
+          anything. Something else? Get started!
+        </p>
+        <div className="content__wrapper">
+          {tasks.map((e, i) => {
+            return (
+              <TaskList
+                title={e.title}
+                items={e.items}
+                key={i}
+                id={i}
+                created={e.isCreated}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,22 @@
 import styles from "./Header.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getForm } from "../../redux/slices/formSlice";
-
+import { CgProfile } from "react-icons/cg";
+import { BsTelegram } from "react-icons/bs";
+import { SlSocialVkontakte } from "react-icons/sl";
+import React from "react";
+import {
+  AiOutlineCloseCircle,
+  AiFillGithub,
+  AiOutlineInstagram,
+} from "react-icons/ai";
 function Header() {
   const dispatch = useDispatch();
   const isCreated = useSelector((state) => {
     return state.form.isCreated;
   });
+
+  const [popupOpened, SetpopupOpened] = React.useState(false);
 
   return (
     <div className={styles.header}>
@@ -14,6 +24,42 @@ function Header() {
       <h1 onClick={() => dispatch(getForm(false))}>
         {isCreated ? "What's the plan?" : ""}
       </h1>
+
+      <div>
+        <p className={styles.abouteMe}>ABOUT ME</p>
+        {!popupOpened && (
+          <CgProfile
+            className={styles.profileIcon}
+            onClick={() => {
+              SetpopupOpened(true);
+            }}
+          />
+        )}
+        {popupOpened ? (
+          <>
+            <a href="https://github.com/jdays2" target="_blank">
+              <AiFillGithub className={styles.one} />
+            </a>
+            <a href="https://vk.com/ostalosmalovremeni" target="_blank">
+              <SlSocialVkontakte className={styles.two} />
+            </a>
+            <a href="https://www.instagram.com/spiritpizza/" target="_blank">
+              <AiOutlineInstagram className={styles.tree} />
+            </a>
+            <a href="https://t.me/Marchezko" target="_blank">
+              <BsTelegram className={styles.four} />
+            </a>
+            <AiOutlineCloseCircle
+              className={styles.exit}
+              onClick={() => {
+                SetpopupOpened(false);
+              }}
+            />
+          </>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 }
