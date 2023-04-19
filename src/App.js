f2dc13a68058a0275eb91eb273f0bc8b00a1e48e
MarkHW <lolka-pop@rambler.ru>
2023-04-19 04:02:39 +0300

    refactor: change styles and some logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import TaskList from "./components/TaskList/TaskList";
 import Header from "./components/Header/Header";
-import videoBg from "./../src/assets/vid/video.mp4";
 
 import Form from "./components/Form/TaskForm";
 
@@ -10,52 +9,39 @@ import { getForm } from "./redux/slices/formSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 function App() {
-  const tasks = useSelector((state) => state.form.tasks);
-  const dispatch = useDispatch();
-  const isCreated = useSelector((state) => state.form.isCreated);
+	const tasks = useSelector((state) => state.form.tasks);
+	const dispatch = useDispatch();
+	const isCreated = useSelector((state) => state.form.isCreated);
 
-  return (
-    <div className="App">
-      <video src={videoBg} autoPlay muted loop />
-      <div class="wrapper">
-        <Header />
-        <Form />
+	return (
+		<div className="App">
+			<div class="wrapper">
+				<Header />
+				<Form />
 
-        <button
-          className={isCreated ? `tryMeButton` : `hidden`}
-          onClick={() => dispatch(getForm(false))}
-        >
-          <span>"THE NEW LIFE ORDER"</span>
-          <BsPlusSquareFill className="addIcon" />
-        </button>
+				<button
+					className={isCreated ? `tryMeButton` : `hidden`}
+					onClick={() => dispatch(getForm(false))}>
+					<span>"THE NEW LIFE ORDER"</span>
+					<BsPlusSquareFill className="addIcon" />
+				</button>
 
-        {tasks[0] ? (
-          ""
-        ) : (
-          <p className="discription">
-            Try to organize your day and make a clear plan of action. You will
-            be able to organize and consciously approach your life, making it
-            more orderly and under your control. With this preparation, you can
-            do anything. Something else? Get started!
-          </p>
-        )}
-
-        <div className="content__wrapper">
-          {tasks.map((e, i) => {
-            return (
-              <TaskList
-                title={e.title}
-                items={e.items}
-                key={i}
-                id={i}
-                created={e.isCreated}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </div>
-  );
+				<div className="content__wrapper">
+					{tasks.map((e, i) => {
+						return (
+							<TaskList
+								title={e.title}
+								items={e.items}
+								key={i}
+								id={i}
+								created={e.isCreated}
+							/>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Form/ItemForm/ItemForm.js
+++ src/components/Form/ItemForm/ItemForm.js
@@ -2,59 +2,64 @@ import styles from "./ItemForm.module.css";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  setCurrentFormItemValue,
-  createNewItem,
-  getItemForm,
+	setCurrentFormItemValue,
+	createNewItem,
+	getItemForm,
 } from "../../../redux/slices/formSlice";
 import { IoCreateOutline } from "react-icons/io5";
 import { CiCircleRemove } from "react-icons/ci";
 
 function ItemForm({ idList }) {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const titleValue = useSelector(
-    (state) => state.form.tasks[idList].currentFormItemValue
-  );
+	const titleValue = useSelector(
+		(state) => state.form.tasks[idList].currentFormItemValue
+	);
 
-  const isCreated = useSelector((state) => state.form.tasks[idList].isCreated);
+	const isCreated = useSelector((state) => state.form.tasks[idList].isCreated);
 
-  const onHandler = (event, idList) =>
-    dispatch(setCurrentFormItemValue({ event, idList }));
+	const onHandler = (event, idList) =>
+		dispatch(setCurrentFormItemValue({ event, idList }));
 
-  return (
-    <div className={!isCreated ? styles.root : styles.hidden}>
-      <form className={styles.form}>
-        <div className={styles.form__content}>
-          <button className={styles.button__wrapper}>
-            <CiCircleRemove
-              className={styles.buttonOne}
-              onClick={(event) => {
-                event.preventDefault();
-                dispatch(getItemForm({ id: idList, value: !isCreated }));
-              }}
-            ></CiCircleRemove>
-          </button>
-          <input
-            maxlength="16"
-            value={titleValue}
-            onChange={(event) => onHandler(event.target.value, idList)}
-            className={styles.imput}
-            type="text"
-            placeholder="What should be done?"
-          />
-          <button className={styles.button__wrapper}>
-            <IoCreateOutline
-              className={styles.buttonTwo}
-              onClick={(event) => {
-                event.preventDefault();
-                dispatch(createNewItem(idList));
-              }}
-            ></IoCreateOutline>
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+	const onKeyDown = (e) => {
+		if (e.keyCode === 13) {
+			dispatch(createNewItem(idList));
+		}
+	};
+
+	return (
+		<div className={!isCreated ? styles.root : styles.hidden}>
+			<form className={styles.form}>
+				<div className={styles.form__content}>
+					{/* <button className={styles.button__wrapper}>
+						<CiCircleRemove
+							className={styles.buttonOne}
+							onClick={(event) => {
+								event.preventDefault();
+								dispatch(getItemForm({ id: idList, value: !isCreated }));
+							}}></CiCircleRemove>
+					</button> */}
+					<input
+						maxlength="16"
+						value={titleValue}
+						onChange={(event) => onHandler(event.target.value, idList)}
+						className={styles.imput}
+						type="text"
+						placeholder="Add the task name and press 'Enter'"
+						onKeyDown={onKeyDown}
+					/>
+					{/* <button className={styles.button__wrapper}>
+						<IoCreateOutline
+							className={styles.buttonTwo}
+							onClick={(event) => {
+								event.preventDefault();
+								dispatch(createNewItem(idList));
+							}}></IoCreateOutline>
+					</button> */}
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default ItemForm;
